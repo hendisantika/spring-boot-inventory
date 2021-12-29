f9f9e1cdc85f38ac09e0ca73e12368de1127dfef
Hendi Santika <hendisantika@gmail.com>
2021-12-29 17:12:04 +0700

    #34 - Add Category Repository

---
+++ src/main/java/com/hendisantika/category/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.hendisantika.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 17.11
+ */
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
