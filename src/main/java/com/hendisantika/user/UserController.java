1fd5d87208e5d211b0c81c98d4ab6c3e6db31723
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:29:40 +0700

    #13 - Add showUserEditForm in UserController

--- src/main/java/com/hendisantika/user/UserController.java
+++ src/main/java/com/hendisantika/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -49,5 +50,16 @@ public class UserController {
     public String saveUser(User user) {
         userRepository.save(user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUserEditForm(@PathVariable("id") Integer id, Model model) {
+        User user = userRepository.findById(id).get();
+        model.addAttribute("user", user);
+
+        List<Role> listRoles = roleRepository.findAll();
+        model.addAttribute("listRoles", listRoles);
+
+        return "user_form";
     }
 }
