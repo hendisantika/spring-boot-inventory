5d956244fbf73fd72eabce00cd25f7050471333f
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:54:16 +0700

    #24 - Add Product Entity

---
+++ src/main/java/com/hendisantika/product/Product.java
@@ -0,0 +1,40 @@
+package com.hendisantika.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 16.53
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+
+    private float price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductDetail> detalles = new ArrayList<>();
+}
