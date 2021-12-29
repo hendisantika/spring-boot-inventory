caa617dea49579de86ddfa7b6179ee9185f7b9bd
Hendi Santika <hendisantika@gmail.com>
2021-12-29 17:08:45 +0700

    #31 - Add showProductModifyForm in ProductController

--- src/main/java/com/hendisantika/product/ProductController.java
+++ src/main/java/com/hendisantika/product/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -64,4 +65,14 @@ public class ProductController {
         return "products";
     }
 
+    @GetMapping("/edit/{id}")
+    public String showProductModifyForm(@PathVariable("id") Integer id, Model model) {
+        Product product = productRepository.findById(id).get();
+        model.addAttribute("product", product);
+
+        List<Category> listCategorias = categoryRepository.findAll();
+        model.addAttribute("listCategories", listaCategorias);
+
+        return "product_form";
+    }
 }
