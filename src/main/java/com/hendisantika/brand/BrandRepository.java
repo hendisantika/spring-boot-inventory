043a2383019c9cc40b98e3379b945809163fb334
Hendi Santika <hendisantika@gmail.com>
2021-12-29 17:18:14 +0700

    #40 - Add Brand Repository

---
+++ src/main/java/com/hendisantika/brand/BrandRepository.java
@@ -0,0 +1,15 @@
+package com.hendisantika.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 17.17
+ */
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
