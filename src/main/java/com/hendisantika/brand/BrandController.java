8c9e4fb8a7d6c212c1c12465097bfa5368b9f050
Hendi Santika <hendisantika@gmail.com>
2021-12-29 17:28:00 +0700

    #45 - Add showModifyMarkForm in BrandController

--- src/main/java/com/hendisantika/brand/BrandController.java
+++ src/main/java/com/hendisantika/brand/BrandController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -50,5 +51,15 @@ public class BrandController {
         model.addAttribute("listBrands", listBrands);
 
         return "brand";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showModifyMarkForm(@PathVariable("id") Integer id, Model model) {
+        List<Category> listCategories = categoryRepository.findAll();
+        Brand brand = brandRepository.findById(id).get();
+
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("brand", brand);
+        return "brand_form";
     }
 }
