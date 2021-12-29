f227b06874ab15c27afbbf30a24170a915473735
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:19:24 +0700

    #5 - Add User Entity

---
+++ src/main/java/com/hendisantika/user/User.java
@@ -0,0 +1,47 @@
+package com.hendisantika.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 16.18
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 10, nullable = false)
+    private String password;
+
+    @ManyToMany // (cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns =
+    @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
