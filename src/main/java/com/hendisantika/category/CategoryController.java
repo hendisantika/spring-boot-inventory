593a1969399d4e4910d4ab157df5b0be8fb165ca
Hendi Santika <hendisantika@gmail.com>
2021-12-29 17:15:57 +0700

    #38 - Add saveCategory in CategoryController

--- src/main/java/com/hendisantika/category/CategoryController.java
+++ src/main/java/com/hendisantika/category/CategoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -34,5 +35,11 @@ public class CategoryController {
     public String showNewCategoryForm(Model model) {
         model.addAttribute("category", new Category());
         return "category_form";
+    }
+
+    @PostMapping
+    public String saveCategory(Category category) {
+        categoryRepository.save(category);
+        return "redirect:/categories";
     }
 }
