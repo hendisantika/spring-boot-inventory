e3992347aaa81588f6989c99b0496a526e15969f
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:25:33 +0700

    #9 - Add userList in UserController

--- src/main/java/com/hendisantika/user/UserController.java
+++ src/main/java/com/hendisantika/user/UserController.java
@@ -2,6 +2,11 @@ package com.hendisantika.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,10 +18,20 @@ import org.springframework.stereotype.Controller;
  * Time: 16.23
  */
 @Controller
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private RoleRepository roleRepository;
+
+    @GetMapping
+    public String userList(Model model) {
+        List<User> listUsers = userRepository.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
+    }
+
 }
