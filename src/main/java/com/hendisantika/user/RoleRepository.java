06d50a8a15b6342098c284d1472cff49aaeeb3ec
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:22:43 +0700

    #6 - Add RoleRepository

---
+++ src/main/java/com/hendisantika/user/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.hendisantika.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 16.22
+ */
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
