f69078750cc4a42fd5c492a386e8f40abcce1c3b
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:18:23 +0700

    #4 - Add Role Entity

---
+++ src/main/java/com/hendisantika/user/Role.java
@@ -0,0 +1,34 @@
+package com.hendisantika.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 16.17
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 25, nullable = false, unique = true)
+    private String name;
+}
