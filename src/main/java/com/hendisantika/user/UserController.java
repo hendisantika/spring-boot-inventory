1bb3267b44647366c10cc913376aa9152878490f
Hendi Santika <hendisantika@gmail.com>
2021-12-29 16:28:15 +0700

    #12 - Add saveUser in UserController

--- src/main/java/com/hendisantika/user/UserController.java
+++ src/main/java/com/hendisantika/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -44,4 +45,9 @@ public class UserController {
         return "user_form";
     }
 
+    @PostMapping
+    public String saveUser(User user) {
+        userRepository.save(user);
+        return "redirect:/users";
+    }
 }
