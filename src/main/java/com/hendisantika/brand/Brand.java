513a019cee69b14571103c0474dbd665d1d2276f
Hendi Santika <hendisantika@gmail.com>
2021-12-29 17:17:31 +0700

    #39 - Add Brand Entity

---
+++ src/main/java/com/hendisantika/brand/Brand.java
@@ -0,0 +1,43 @@
+package com.hendisantika.brand;
+
+import com.hendisantika.category.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-inventory
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/21
+ * Time: 17.16
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "brand_id")
+    private List<Category> categories = new ArrayList<>();
+
+}
