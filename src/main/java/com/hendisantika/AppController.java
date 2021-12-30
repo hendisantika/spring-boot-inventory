501e2a16b4e71125b4453a497a2cd91a08f8a187
Hendi Santika <hendisantika@gmail.com>
2021-12-30 07:39:27 +0700

    #53 - Init Roles Data

--- src/main/java/com/hendisantika/AppController.java
+++ src/main/java/com/hendisantika/AppController.java
@@ -1,7 +1,14 @@
 package com.hendisantika;
 
+import com.hendisantika.user.Role;
+import com.hendisantika.user.RoleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,7 +20,21 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Time: 17.31
  */
 @Controller
+@Slf4j
 public class AppController {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @PostConstruct
+    public void initRoles() {
+        roleRepository.saveAll(
+                Arrays.asList(
+                        new Role(1, "ADMIN"),
+                        new Role(2, "USER"),
+                        new Role(3, "MARKETING")));
+        log.info("Init Roles");
+    }
+
     @GetMapping({"", "/"})
     public String viewHomePage() {
         return "index";
